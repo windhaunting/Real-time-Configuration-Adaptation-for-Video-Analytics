fd7474cde5dd37b7c91b3045cb2e8b253f11def6
Tian Zhou <yanxiangtianji@gmail.com>
2019-11-06 11:56:28 -0500

    add key point operation functions

--- tian/utilPose.py
+++ tian/utilPose.py
@@ -8,7 +8,63 @@ Created on Fri Oct 25 02:33:57 2019
 import numpy as np
 
 
-# -------- part 1: pose fill --------
+# -------- part 1: key point operation --------
+
+'''
+0	nose
+1	leftEye
+2	rightEye
+3	leftEar
+4	rightEar
+5	leftShoulder
+6	rightShoulder
+7	leftElbow
+8	rightElbow
+9	leftWrist
+10	rightWrist
+11	leftHip
+12	rightHip
+13	leftKnee
+14	rightKnee
+15	leftAnkle
+16	rightAnkle
+'''
+
+__KPT_NAME__ = ['nose', 'leftEye', 'rightEye', 'leftEar', 'rightEar',
+               'leftShoulder', 'rightShoulder', 'leftElbow', 'rightElbow',
+               'leftWrist', 'rightWrist', 'leftHip', 'rightHip',
+               'leftKnee', 'rightKnee', 'leftAnkle', 'rightAnkle']
+
+__KPT_MAP__ = {'nose': 0,
+               'leftEye':1, 'leye':1, 'ly':1, 'rightEye':2, 'reye':2, 'ry':2,
+               'leftEar':3, 'lear':3, 'le':3, 'rightEar':4, 'rear':4, 're':4,
+               'leftShoulder':5, 'lshoulder':5, 'ls':5,
+               'rightShoulder':6, 'rshoulder':6, 'rs':6,
+               'leftElbow':7, 'lelbow':7, 'lb':7, 'rightElbow':8, 'relbow':8, 'rb':8,
+               'leftWrist':9, 'lwrist':9, 'lw':8, 'rightWrist':10, 'rwrist':10, 'rw':10,
+               'leftHip':11, 'lhip':11, 'lh':11, 'rightHip':12, 'rhip':12, 'rh':12,
+               'leftKnee':13, 'lknee':13, 'lk':13, 'rightKnee':14, 'rknee':14, 'rk':14,
+               'leftAnkle':15, 'lankle':15, 'la':15, 'rightAnkle':15, 'rankle':16, 'ra':16}
+
+__NUM_KPT__ = 17
+
+def name2kp(data):
+    if isinstance(data, str):
+        return __KPT_MAP__[data]
+    elif isinstance(data, tuple):
+        return tuple([name2kp(v) for v in data])
+    elif isinstance(data, list):
+        return [name2kp(v) for v in data]
+    elif isinstance(data, np.ndarray) and data.ntype.kind in ['U','S','a']:
+        s=data.shape
+        res=np.apply_along_axis(lambda n:__KPT_MAP__[n], 0, data.ravel())
+        return res.reshape(s)
+    else:
+        assert hasattr(data, '__iter__')
+        return [name2kp(v) for v in data]
+
+
+# -------- part 2: pose matrix operation --------
 
 def fillUnseen(kpm, method='same'):
     assert kpm.dim == 4
@@ -40,23 +96,24 @@ def fillUnseen(kpm, method='same'):
                     res[x,y,i,:2] = res[x,p,i,:2] + s / (p-q) * (y-p)
     return res
 
-# -------- part 2: OKS --------
+# -------- part 3: OKS --------
+    
 
 __KPT_OKS_SIGMAS__ = np.array([.26, .25, .25, .35, .35, .79, .79, .72, .72, .62,.62, 1.07, 1.07, .87, .87, .89, .89])/10.0
-__NUM_KPT__ = 17
 
-'''
-Object Keypoint Similarity
-http://cocodataset.org/#keypoints-eval
-OKS = Σi[exp(-di2/2s2κi2)δ(vi>0)] / Σi[δ(vi>0)]
-https://github.com/cocodataset/cocoapi/blob/master/PythonAPI/pycocotools/cocoeval.py
-input:
-    <gts>: ground truth key points. Format: 2d array with shape (17,3) for the (x,y) coordinates of the 17 keypoints.
-    <dts>: destination key points. Format: the same as <gts>.
-output:
-    a scalar of oks
-'''
+
 def computeOKS_pair(gts, dts, sigmas = None):
+    '''
+    Object Keypoint Similarity
+    http://cocodataset.org/#keypoints-eval
+    OKS = Σi[exp(-di2/2s2κi2)δ(vi>0)] / Σi[δ(vi>0)]
+    https://github.com/cocodataset/cocoapi/blob/master/PythonAPI/pycocotools/cocoeval.py
+    input:
+        <gts>: ground truth key points. Format: 2d array with shape (17,3) for the (x,y) coordinates of the 17 keypoints.
+        <dts>: destination key points. Format: the same as <gts>.
+    output:
+        a scalar of oks
+    '''
     assert isinstance(gts, np.ndarray) and gts.shape == (17,3)
     assert isinstance(dts, np.ndarray) and dts.shape == (17,3)
     sigmas = np.array(__KPT_OKS_SIGMAS__ if sigmas is None else sigmas)
