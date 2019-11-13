e2fdc34947009f0a0aebf9d12bff07b25fc94baa
Tian Zhou <yanxiangtianji@gmail.com>
2019-11-13 15:00:58 -0500

    add function for generating confs for multiple FPS

--- tian/fpsconv.py
+++ tian/fpsconv.py
@@ -7,6 +7,7 @@ Created on Tue Oct 29 10:09:56 2019
 
 import numpy as np
 import utilPose
+import preprocess
 
 
 # -------- part 1: methods for sampling frames when converting FPS --------
@@ -84,10 +85,12 @@ class FrameSampler:
             rt = sum(sfx<r) - 1
         return m*self.tgtFps + rt
 
+
 # -------- part 2: convert fps --------
 
-def convertFPS(kpm, ptm, tgtFps, srcFps=25, offset=0,
-               method='keep', refConf=0, alpha=0.8):
+
+def convertFPS(kpm, ptm, tgtFps, srcFps=25, offset=0, refConf=0,
+               method='keep', alpha=0.8):
     '''
     Convert OKS and SPF to another frame-rate by sampling frames
     input:
@@ -96,9 +99,9 @@ def convertFPS(kpm, ptm, tgtFps, srcFps=25, offset=0,
         <tgtFps> target frame rate (frame/second)
         <srcFps> the frame rate (frame/second) of input data
         <offset> start from the <offset>-th frame
+        <refConf> (for keep method) the configuration used as OKS reference
         <method> method for estimating pose of the unprocessed frames.
             including: keep-old-pose, linear, exponential average
-        <refConf> (for keep method) the configuration used as OKS reference
         <alpha> (for eam method) the factor for keeping lastest value
     output:
         <roks> 2d (conf-frame) OKS matrix for the target frame rate (average oks)
@@ -119,8 +122,9 @@ def convertFPS(kpm, ptm, tgtFps, srcFps=25, offset=0,
     roks = np.zeros((n, mt))
     rptm = np.zeros((n, mt))
     if method != 'keep':
-        kpmFill = utilPose.fillUnseen(kpm, 'linear')
-        last = kpmFill[:,0,:,0:2]
+        #kpmFill = utilPose.fillUnseen(kpm, 'linear')
+        #last = kpmFill[:,0,:,0:2]
+        last = kpm[:,0,:,0:2]
     if method == 'eam':
         speedOld = np.zeros_like(last)
         assert 0 <= alpha <= 1
@@ -129,9 +133,7 @@ def convertFPS(kpm, ptm, tgtFps, srcFps=25, offset=0,
         span = sfi[i % tgtFps]
         ftouse = range(fused, fused+span)
         pose=kpm[:,ftouse,:,:]
-        if method == 'keep':
-            oksm = utilPose.computeOKS_mat(kpm[refConf,fused], pose)
-        else: # linear and eam
+        if method != 'keep': # linear and eam
             #speed = diff[:,ftouse,:,:].mean(1)
             speed = (kpm[:,fused,:,0:2] - last) / sfi[(i-1) % tgtFps]
             last = kpm[:,fused,:,0:2]
@@ -141,19 +143,19 @@ def convertFPS(kpm, ptm, tgtFps, srcFps=25, offset=0,
             if span > 1:
                 delta = np.stack([speed*i for i in range(1,span)], axis=1)
                 pose[:,1:,:,[0,1]] += delta
-            oksm = utilPose.computeOKS_pairMat(kpm[:,ftouse], pose)
+        oksm = utilPose.computeOKS_mat(kpm[refConf,fused], pose)
         roks[:,i] = oksm.mean(1)
         rptm[:,i] = ptm[:,i]
         fused += span
     return roks, rptm
 
 
-# -------- part 3: unify to time unit to second--------
+# -------- part 3: segment to some time unit(second) --------
 
 
-def mergeData(oksm, ptm, unit):
+def segmentData(oksm, ptm, unit):
     '''
-    Unify to second. If the last second is not complete, it is cut off.
+    Unify to second or some other unit. If the last second is not complete, it is cut off.
     Input:
         <oksm> object keypoint similarity matrix (2d or 1d: (conf)-frame)
         <ptm> processing time matrix (2d or 1d: (conf)-frame)
@@ -181,4 +183,66 @@ def mergeData(oksm, ptm, unit):
         resOks = np.mean(oksm.reshape(k, -1, unit), 2)
         resPtm = np.sum(ptm.reshape(k, -1, unit), 2)
     return resOks, resPtm
+
+
+# -------- part 4: generate multiple-fps configurations --------
+
+
+def generateConfsFPS(kpm, ptm, fpsList, srcFps=25, segSec=1, method='eam:0.8',
+                     refConf=0):
+    '''
+    Generate configurations under different FPS and unify them to segments
+    New configurations are stacked in group on the first dimension.
+    Input:
+        <kpm> the original key-point matrix (4d or 3d: (conf)-frame-kp-xyv)
+        <ptm> the original processing-time matrix (2d or 1d: (conf)-frame)
+        <fpsList> the FPS list to use
+        <srcFps> the FPS of the original matrices
+        <segSec> how many second per segment
+        <method> method of extrapolate poses
+        <refConf> (for keep method) the configuration used as OKS reference
+    Output:
+        <oks> oks matrix (4d)
+        <ptm> processing time matrix (4d)
+    '''
+    assert kpm.ndim == 4 or kpm.ndim == 3
+    assert kpm.shape[-2:] == (17,3)
+    assert kpm.shape[:-2] == ptm.shape
+    assert len(fpsList) > 0
+    assert isinstance(segSec,int) and 1 <= segSec
+    assert method in ['keep', 'linear', 'eam'] or method[:4] == 'eam:'
+    if kpm.ndim == 3:
+        kpm.resize(1,-1,17,3)
+        ptm.resize(1,-1)
+    # eam
+    if method[:4] == 'eam:' and len(method) > 4:
+        alpha = float(method[4:])
+        method = 'eam'
+    else:
+        alpha = 0.8
+    # result
+    roks = [None for i in range(len(fpsList))]
+    rptm = [None for i in range(len(fpsList))]
+    for i in range(len(fpsList)):
+        tgtFps = fpsList[i]
+        if tgtFps != srcFps:
+            o, p = convertFPS(kpm, ptm, fpsList[i], srcFps, refConf=refConf,
+                              method=method, alpha=alpha)
+        else:
+            o = preprocess.pose2oks(kpm, refID=refConf)
+            p = ptm
+        roks[i], rptm[i] = segmentData(o, p, tgtFps*segSec)
+    # align
+    m = min(o.shape[1] for o in roks)
+    for i in range(len(fpsList)):
+        if roks[i].shape[1] != m:
+            roks[i] = roks[i][:,:m]
+            rptm[i] = rptm[i][:,:m]
+    # stack on the first dimension
+    roks = np.vstack(roks)
+    rptm = np.vstack(rptm)
+    return roks, rptm
+    
+
+__FPS_LIST__ = [1, 2, 5, 10, 15, 20, 25]
 
